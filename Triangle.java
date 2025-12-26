b4301a9f9844eae51799007342a1529897c8adb6
maria2080377 <masha900p@gmail.com>
2025-12-26 13:38:43 +0300

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -6,12 +6,12 @@ public class Triangle {
     private double sideC;
 
     public Triangle(double sideA, double sideB, double sideC) {
-        // Проверяем, может ли существовать треугольник с такими сторонами
+       
         if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
             throw new RuntimeException("There is no figure with such parameters.");
         }
 
-        // Проверка неравенства треугольника
+       
         if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA) {
             throw new RuntimeException("There is no figure with such parameters.");
         }
@@ -22,7 +22,7 @@ public class Triangle {
     }
 
     public double getArea() {
-        // Формула Герона
+       
         double p = getPerimeter() / 2;
         return Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
     }
@@ -36,4 +36,5 @@ public class Triangle {
         return String.format("Треугольник [стороны=%.2f,%.2f,%.2f, площадь=%.2f, периметр=%.2f]",
                 sideA, sideB, sideC, getArea(), getPerimeter());
     }
+
 }
